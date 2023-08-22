2b617c48efe494f0ae264231f4209954e3cd0029
frntml <134443996+frntml@users.noreply.github.com>
2023-08-22 18:23:10 -0300

    Add files via upload

---
+++ ejercicio5/src/ejercicio5/Program.java
@@ -0,0 +1,11 @@
+package ejercicio5;
+
+public class Program {
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3};
+		for(int i : array) {
+			System.out.println(i);
+			
+		}
+	}
+}
---
+++ ejercicio5/src/ejercicio5/ejercicio5.java
@@ -0,0 +1,13 @@
+package ejercicio5;
+
+public class Program {
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3};
+		for(int elemento : array) {
+			System.out.println(elemento);
+		}
+	}
+}
+
+//este programa toma cada elemento del array y lo imprime, 
+//reemplazando el valor de la variable "elemento" por cada elemento del array
